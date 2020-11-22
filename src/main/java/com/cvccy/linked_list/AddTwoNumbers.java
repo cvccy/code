09d67d8ae4e245f9758b1287c4ccab2bfa28efe7
kewei <x@hanff.cn>
2020-11-22 17:04:57 +0800

    Initial commit

---
+++ src/main/java/com/cvccy/linked_list/AddTwoNumbers.java
@@ -0,0 +1,77 @@
+package com.cvccy.linked_list;
+
+
+/**
+ * 给出两个非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode head = null, tail = null; //分别指向新结点的首尾
+        int flag = 0;   //标志进位
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + flag;    //求和
+            if (head == null) {          //创建新节点
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            flag = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (flag == 1){   //l1 l2 循环结束，如果进位为1，则创建进位节点
+            tail.next = new ListNode(flag);
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode l1 = new ListNode(2,new ListNode(4,new ListNode(3)));
+        ListNode l2 = new ListNode(5,new ListNode(6,new ListNode(4)));
+        ListNode result = addTwoNumbers.addTwoNumbers(l1,l2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+
+
+    }
+
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+
+
+}
