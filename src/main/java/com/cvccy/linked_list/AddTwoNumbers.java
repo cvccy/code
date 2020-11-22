69b14f5a0ff412035ad3bc4e8d18429cac523cb9
cvccy <710723333@qq.com]>
2020-11-22 17:16:01 +0800

    2.两数之和

--- src/main/java/com/cvccy/linked_list/AddTwoNumbers.java
+++ src/main/java/com/cvccy/linked_list/AddTwoNumbers.java
@@ -2,7 +2,7 @@ package com.cvccy.linked_list;
 
 
 /**
- * 给出两个非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
+ * 2、给出两个非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
  *
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  *
@@ -10,9 +10,9 @@ package com.cvccy.linked_list;
  *
  * 示例：
  *
- * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
- * 输出：7 -> 0 -> 8
- * 原因：342 + 465 = 807
+ * * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * * 输出：7 -> 0 -> 8
+ * * 原因：342 + 465 = 807
  *
  */
 public class AddTwoNumbers {
@@ -45,12 +45,32 @@ public class AddTwoNumbers {
         return head;
     }
 
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1), tail = head;
+        int t = 0;
+        while (l1 != null || l2 != null || t != 0) {
+            if (l1 != null) {
+                t += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                t += l2.val;
+                l2 = l2.next;
+            }
+            tail.next = new ListNode(t % 10);
+            tail = tail.next;
+            t /= 10;
+        }
+
+        return head.next;
+    }
+
     public static void main(String[] args) {
 
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
         ListNode l1 = new ListNode(2,new ListNode(4,new ListNode(3)));
         ListNode l2 = new ListNode(5,new ListNode(6,new ListNode(4)));
-        ListNode result = addTwoNumbers.addTwoNumbers(l1,l2);
+        ListNode result = addTwoNumbers.addTwoNumbers2(l1,l2);
         while (result != null) {
             System.out.println(result.val);
             result = result.next;
